5e21ff54c6a6a332b5a7dfc8e511df89037e3957
Charles Guertin <charlesguertin@live.ca>
2018-04-12 19:14:02 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -19,17 +19,32 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined!'
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('Create Message',newMessage);
         io.emit('newMessage', {
             ...newMessage,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     ...newMessage,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect',() => {
         console.log('User is disconnected');
-    })
+    });
 });
 
 server.listen(port, () => {
