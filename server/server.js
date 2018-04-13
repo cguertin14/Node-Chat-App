8898b3fe6b6fa7901577f6574debfc53f6cf21cc
Charles Guertin <charlesguertin@live.ca>
2018-04-13 17:19:31 -0400

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection',(socket) => {
     console.log('New user connected');
 
     socket.on('join', (params,callback) => {
-        if (!isRealString(params.name) || !isRealString(params.room)) {
+        if (!isRealString(params.room)) {
             return callback('Name and room name are required.')
         }
 
@@ -39,13 +39,21 @@ io.on('connection',(socket) => {
     });
 
     socket.on('createMessage', (newMessage, callback) => {
-        console.log('Create Message',newMessage);
-        io.emit('newMessage', generateMessage(newMessage.from,newMessage.text));
+        let user = users.getUser(socket.id);
+
+        if (user && isRealString(newMessage.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name,newMessage.text));
+        }
+
         callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude, coords.longitude));
+        let user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('disconnect',() => {
